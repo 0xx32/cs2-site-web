56d676089fbf1c1292fb581bef3656f3f8112d1c
0xx32 <dimas1523@mail.ru>
2025-11-04 10:22:28 +0300

    init

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Provider from "./provider.vue";
+</script>
+
+<template>
+    <Provider>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </Provider>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
+
+  imports: {
+    autoImport: true,
+  },
+  typescript: {
+    typeCheck: true,
+  },
+});
